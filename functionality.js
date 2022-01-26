7745eef586fa56ad89ba922688991403be8c8d36
noobie100 <98452266+noobie100@users.noreply.github.com>
2022-01-26 16:55:19 +0000

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -3,9 +3,17 @@ var hgt=window.screen.height;
 var wdt=window.screen.width;
 var hgt_scaled=hgt*scale;
 var wdt_scaled=wdt*scale;
-document.getElementById("res_container").innerHTML="<b>"+wdt_scaled+" x "+hgt_scaled+"</b><br>";
+var r=wdt_scaled/hgt_scaled;
+console.log(r);
+document.getElementById("res_container").innerHTML="<b>"+wdt_scaled+" x "+hgt_scaled+"</b><br><br>";
 
 document.getElementById("main").innerHTML=document.getElementById('op1').innerHTML;
+
+var output=document.getElementById('output');
+output.style.height= hgt_scaled/6+'px';
+output.style.width=wdt_scaled/6+'px';
+console.log(output.style.height);
+console.log(output.style.width);
 
 function show(parameter_id){
     document.getElementById("canvas").innerHTML="";
@@ -28,7 +36,8 @@ function size2dpi(){
     var d = document.getElementById("size").value;
     dpi= Math.sqrt(hgt_scaled*hgt_scaled+wdt_scaled*wdt_scaled)/d;
     console.log(dpi);
-    document.getElementById("canvas").innerHTML="<b>"+Math.round(dpi)+"</b>";
+    document.getElementById("result_text").innerHTML="<b>"+Math.round(dpi)+"</b>";
+
 }
 
 function dpi2size(){
@@ -38,5 +47,5 @@ function dpi2size(){
     var width=wdt_scaled/dpi;
     var size=Math.sqrt(height*height + width*width);
     console.log(size);
-    document.getElementById("canvas").innerHTML="<b>"+Math.round(size*10)/10+" inches</b>";
+    document.getElementById("result_text").innerHTML="<b>"+Math.round(size*10)/10+" inches</b>";
 }
